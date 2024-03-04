459efb0460c2aaeffd4d257729eb9d77d19bfba5
Miguel Felix Cazarez <755699@student.dusd.net>
2024-03-04 15:26:52 -0800

    On bonuses
    On bonuses

--- Unit1FelixP7/Assets/Scripts/PlayerController.cs
+++ Unit1FelixP7/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float fowardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +18,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        fowardInput = Input.GetAxis("Vertical");
+        // Moves the car foward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        // Rotates the car based on horizontal input 
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput *  Time.deltaTime);
 
     }
 }
